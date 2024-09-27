8a7f94ef5b51985a1b0414efaa3ecc974aae9444
OkeLDF <otaviofolador43@gmail.com>
2024-09-26 22:51:23 -0300

    Update PagamentoController.java

--- futebol/src/main/java/dw/trabalho/futebol/control/PagamentoController.java
+++ futebol/src/main/java/dw/trabalho/futebol/control/PagamentoController.java
@@ -1,12 +1,37 @@
 package dw.trabalho.futebol.control;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dw.trabalho.futebol.model.Pagamento;
 import dw.trabalho.futebol.repository.PagamentoRepository;
 
 @RestController
 public class PagamentoController {
     @Autowired
     PagamentoRepository rep;
+
+    @GetMapping("/pagamento/")
+    public ResponseEntity<List<Pagamento>> getAllPagamentos() {
+        try {
+            List<Pagamento> pagamentos = new ArrayList<>();
+
+            rep.findAll().forEach(pagamentos::add);
+
+            if(pagamentos.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(pagamentos, HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
